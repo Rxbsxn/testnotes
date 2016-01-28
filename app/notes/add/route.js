5bf1ce1b08271eaadceb67734d52372b2bfe0f6b
Michał Czyż <michalczyz@MacBook-Pro-Micha.local>
2016-01-28 18:40:09 +0100

    notes listing

--- app/notes/add/route.js
+++ app/notes/add/route.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    redirectToNote(note) {
-      this.transitionTo('note', note);
+    redirectToNote() {
+      this.transitionTo('notes');
     },
   },
 });
